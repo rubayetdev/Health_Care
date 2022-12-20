8d0f037f407de4ad61c0a1e547c25cb4a6255fda
rubayet <rubayetislam16@gmail.com>
2022-12-20 11:53:29 +0600

    Rubayet

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -3,20 +3,41 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private TextView namText, descriptionText;
+    private TextView namText;
+    private Button btn,btns;
+    int count = 0;
+    int cunt = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        namText = (TextView) findViewById(R.id.nameText);
-        namText.setText("Developed by:");
-        descriptionText = (TextView) findViewById(R.id.dText);
-        descriptionText.setText("Sheikh Md. Rubayet");
+        btn = (Button) findViewById(R.id.btnLog);
+        btns = (Button) findViewById(R.id.btnOut);
+        namText = (TextView) findViewById(R.id.dText);
+
+        btn.setOnClickListener(this);
+        btns.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view.getId() == R.id.btnLog){
+            count++;
+            namText.setText("Login " +count+ " times");
+
+        }
+        else{
+            cunt++;
+        namText.setText("Logout " +cunt+ " times");
+        }
     }
 }
